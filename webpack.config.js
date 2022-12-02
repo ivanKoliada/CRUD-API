c1061ad7ef675b39ebde8cef44afb0830c717e35
justgrace <ivan.koliada69@gmail.com>
2022-12-02 20:38:29 +0300

    init: start crud api project

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+import path from 'path';
+
+module.exports = {
+  mode: 'production',
+  target: 'node',
+  entry: {
+    main: './src/index.ts',
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+      },
+    ],
+  },
+};
