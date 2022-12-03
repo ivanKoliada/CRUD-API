4a0a2c51e04f12557740deafb4728341f898c454
justgrace <ivan.koliada69@gmail.com>
2022-12-03 20:42:30 +0300

    feat: validate url

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,40 +1,23 @@
 import { IncomingMessage, ServerResponse } from 'http';
 
 import { getUsers, getUser, createUser, updateUser, deleteUser } from '../controllers';
+import { validateUrl } from '../utils';
 import { ENDPOINT, METHODS, STATUS } from '../constants';
 
 export const serviceController = async (request: IncomingMessage, response: ServerResponse) => {
+  const isUrlValid = validateUrl(request);
+  const id = request.url?.split('/')[3] as string;
+
   if (request.url === ENDPOINT && request.method === METHODS.GET) {
     getUsers(response);
-  } else if (
-    request.url?.match(
-      /\/api\/users\/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/,
-    ) &&
-    request.method === METHODS.GET
-  ) {
-    const id = request.url.split('/')[3];
-
+  } else if (isUrlValid && request.method === METHODS.GET) {
     getUser(response, id);
   } else if (request.url === ENDPOINT && request.method === METHODS.POST) {
     createUser(request, response);
-  } else if (
-    request.url?.match(
-      /\/api\/users\/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/,
-    ) &&
-    request.method === METHODS.PUT
-  ) {
-    const id = request.url.split('/')[3];
-
+  } else if (isUrlValid && request.method === METHODS.PUT) {
     updateUser(request, response, id);
-  } else if (
-    request.url?.match(
-      /\/api\/users\/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/,
-    ) &&
-    request.method === METHODS.DELETE
-  ) {
-    const id = request.url.split('/')[3];
-
-    deleteUser( response, id);
+  } else if (isUrlValid && request.method === METHODS.DELETE) {
+    deleteUser(response, id);
   } else {
     response.writeHead(STATUS.BAD_REQUEST, {
       'content-type': 'application/json',
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,11 @@
 import { IncomingMessage } from 'http';
 
+export const validateUrl = (request: IncomingMessage) => {
+  return request.url?.match(
+    /\/api\/users\/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/,
+  );
+};
+
 export const getPostData = (request: IncomingMessage) => {
   return new Promise<void>((resolve, reject) => {
     try {
